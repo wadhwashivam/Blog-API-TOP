e4f1a417281add26d81343f24faf31a4aefdcb81
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-08-01 12:42:10 -0400

    Styling for client admin finished

--- client-admin/js/index.js
+++ client-admin/js/index.js
@@ -6,13 +6,21 @@ if (!getToken()){
 
         posts.forEach(post => {
             const listItem = document.createElement("li");
-            const link = document.createElement("a");
+            listItem.className = "list-row";
 
+            const link = document.createElement("a");
             link.href = `editPost.html?id=${post.id}`;
             link.textContent = post.title;
+            link.className = "link link-hover font-medium"
 
-            if (!post.published){            
-                link.style.color = "grey";
+            if (!post.published){
+                link.classList.add("opacity-50");
+                
+                const badge = document.createElement("span");
+                badge.className = "badge badge-ghost badge-sm ml-2";
+                badge.textContent = "Draft";
+
+                link.appendChild(badge);
             }
 
             listItem.appendChild(link);
